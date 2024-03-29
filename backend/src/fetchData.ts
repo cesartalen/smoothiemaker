34e47b21ada2b3e6221cd41e3de1d488fb9240dc
cesartalen <cesartalense@gmail.com>
2024-03-30 00:50:31 +0100

    Rename

---
+++ backend/src/fetchData.ts
@@ -0,0 +1,15 @@
+import { writeFile } from 'fs'
+
+
+fetch('https://fruityvice.com/api/fruit/all')
+  .then(response => response.json())
+  .then(data => {
+    writeFile('fruit-data.json', JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Data has been fetched and written to fruit-data.json')
+      }
+    })
+  })
+  .catch(error => console.error(error))
