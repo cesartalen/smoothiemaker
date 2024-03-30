046a407b00ca123d036d14f04f4852e290fc954d
cesartalen <cesartalense@gmail.com>
2024-03-30 20:25:35 +0100

    dbConfig ES6 import. added type casting and default port value

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -1,9 +1,11 @@
-const Pool = require('pg').Pool
+import pg from 'pg'
+import 'dotenv/config'
 
-export const pool = new Pool({
+
+export const pool = new pg.Pool({
   user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD as string,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 5432,
   database: process.env.DB_DATABASE 
 })
