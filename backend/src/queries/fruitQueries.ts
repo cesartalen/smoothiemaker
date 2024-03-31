fce35289cf393f6ee5001966f03fdd89141607cf
cesartalen <cesartalense@gmail.com>
2024-03-31 13:12:14 +0200

    Add getFruitsAndNutrition and getFruitAndNutrition(name) queries

--- backend/src/queries/fruitQueries.ts
+++ backend/src/queries/fruitQueries.ts
@@ -16,3 +16,14 @@ export const getFruits = () => {
   return 'SELECT * FROM fruit'
 }
 
+
+export const getFruitsAndNutrition = () => {
+  return 'SELECT * FROM fruit JOIN fruit_nutrition ON fruit.id = fruit_nutrition.fruit_id'
+}
+
+export const getFruitAndNutrition = (fruit: string) => {
+  return {
+    text: 'SELECT * FROM fruit JOIN fruit_nutrition ON fruit.id = fruit_nutrition.fruit_id WHERE name = $1',
+    values: [fruit],
+  }
+}
