b58d8bae94f4b68849a8746afdd9abdbfbc5fba3
cesartalen <cesartalense@gmail.com>
2024-03-31 19:27:57 +0200

    Add new routes to fruitRoutes

--- backend/src/routes/fruitRoutes.ts
+++ backend/src/routes/fruitRoutes.ts
@@ -1,6 +1,10 @@
 import express from 'express'
-import { getFruit } from '../controllers/fruitController.js'
+import * as fruitControllers from '../controllers/fruitController.js'
 
 export const router = express.Router()
 
-router.get('/fruit/:fruit', getFruit)
+router.get('/fruit/:fruit', fruitControllers.getFruit)
+router.get('/fruits', fruitControllers.getFruits) 
+router.get('/fruit-nutrition/:fruitId', fruitControllers.getFruitNutrition)
+router.get('/fruit-and-nutrition/:fruit', fruitControllers.getFruitAndNutrition)
+router.get('/fruits-and-nutrition', fruitControllers.getFruitsAndNutrition)
