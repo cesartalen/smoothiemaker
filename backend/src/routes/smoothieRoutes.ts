e3c7c079fbc3debb6c39cdfeb189075a39354b05
cesartalen <cesartalense@gmail.com>
2024-03-31 23:17:09 +0200

    Add post route for smoothie

--- backend/src/routes/smoothieRoutes.ts
+++ backend/src/routes/smoothieRoutes.ts
@@ -1,3 +1,6 @@
 import express from 'express'
+import * as smoothieControllers from '../controllers/smoothieController.js'
 
 export const router = express.Router()
+
+router.post('/smoothie-mix', smoothieControllers.getSmoothie)
