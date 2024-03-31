e2c5020c0eebe286b7ad049751b8fd05a3016c4b
cesartalen <cesartalense@gmail.com>
2024-04-01 00:59:57 +0200

    Add input validation for getSmoothie function

--- backend/src/controllers/smoothieController.ts
+++ backend/src/controllers/smoothieController.ts
@@ -5,8 +5,12 @@ import { NutrientType } from '../types/fruit.js'
 import { SmoothieType, FruitsType } from '../types/smoothie.js'
 
 export const getSmoothie = async (req: Request, res: Response) => {
+  if(req.body.fruits === undefined || req.body.fruits.length === 0) {
+    return res.json({ message: 'No ingredients provided!' })
+  }
+  
   const fruits: SmoothieType = req.body
-
+  
   let nutritionContent : NutrientType = {
     calories: 0,
     fat: 0,
@@ -14,13 +18,17 @@ export const getSmoothie = async (req: Request, res: Response) => {
     protein: 0,
     carbohydrates: 0
   }
-
+  
   for (const fruit of fruits.fruits) {
     const query = smoothieQueries.getOnlyFruitNutritionQuery(fruit.name)
     const result = await pool.query(query)
-
+    
+    if(result.rowCount === 0) {
+      return res.json({ message: `No fruit called ${fruit.name} exists in this database` })
+    }
+    
     let nutrition = result.rows[0]
-
+    
     for(const nutrient in nutrition) {
       nutritionContent[nutrient as keyof NutrientType] += nutrition[nutrient] * fruit.amount
     }
