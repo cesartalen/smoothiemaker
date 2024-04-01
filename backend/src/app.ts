cde25e483604dfa2568dfb26db7a37d6990168cd
cesartalen <cesartalense@gmail.com>
2024-04-01 21:09:59 +0200

    Add use errorhandling middleware in app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router as smoothieRouter } from './routes/smoothieRoutes.js'
 import { router as fruitRouter } from './routes/fruitRoutes.js'
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,6 +10,8 @@ app.use(express.json())
 
 app.use('/smoothies', smoothieRouter)
 app.use('/fruits', fruitRouter)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
