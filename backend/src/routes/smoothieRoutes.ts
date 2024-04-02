6cdda7d05d7e4a1626e616c88f097661bbf9bfe6
cesartalen <cesartalense@gmail.com>
2024-04-02 22:06:33 +0200

    Add route for createSmoothie function

--- backend/src/routes/smoothieRoutes.ts
+++ backend/src/routes/smoothieRoutes.ts
@@ -5,3 +5,4 @@ export const router = express.Router()
 
 // Routes for retrieving smoothies, or nutritional data for combinations of fruits (smoothie)
 router.post('/smoothie-mix', smoothieControllers.getSmoothie)
+router.post('/create-smoothie', smoothieControllers.createSmoothie)
