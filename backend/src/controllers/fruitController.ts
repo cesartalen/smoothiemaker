42fd89d7b7a9a192fd47f765f81d2254119b3463
cesartalen <cesartalense@gmail.com>
2024-04-03 14:27:57 +0200

    Modify functions to use catchAsync

--- backend/src/controllers/fruitController.ts
+++ backend/src/controllers/fruitController.ts
@@ -2,11 +2,12 @@ import { NextFunction, Request, Response } from 'express'
 import { pool } from '../config/dbConfig.js'
 import * as fruitQueries from '../queries/fruitQueries.js'
 import { ResponseStatus } from '../utils/responseStatus.js'
+import { catchAsync } from '../utils/catchAsync.js'
 
 // Controller functions for fruit routes
 
 // Get a single fruit by its (name)
-export const getFruit = async (req: Request, res: Response, next: NextFunction) => {
+export const getFruit = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const { fruit } = req.params
   if(!fruit) { 
     return next({
@@ -24,16 +25,16 @@ export const getFruit = async (req: Request, res: Response, next: NextFunction) 
   }
   
   res.json(result.rows)
-}
+})
 
 // Get all fruits ()
-export const getFruits = async (req: Request, res: Response) => {
+export const getFruits = catchAsync(async (req: Request, res: Response) => {
   const result = await pool.query(fruitQueries.getFruitsQuery())
   res.json(result.rows)
-}
+})
 
 // Get the nutritional data for a single fruit by its (fruitId)
-export const getFruitNutrition = async (req: Request, res: Response) => {
+export const getFruitNutrition = catchAsync(async (req: Request, res: Response) => {
   const { fruitId } = req.params
   if(!fruitId) { 
     return res.json({ message: 'No fruit provided' })
@@ -45,10 +46,10 @@ export const getFruitNutrition = async (req: Request, res: Response) => {
 
   const result = await pool.query(fruitQueries.getFruitNutritionQuery(parseInt(fruitId)))
   res.json(result.rows)
-}
+})
 
 // Get the nutritional data for a single fruit by its (name)
-export const getFruitAndNutrition = async (req: Request, res: Response) => {
+export const getFruitAndNutrition = catchAsync(async (req: Request, res: Response) => {
   const { fruit } = req.params
   if(!fruit) { 
     return res.json({ message: 'No fruit provided' })
@@ -56,10 +57,10 @@ export const getFruitAndNutrition = async (req: Request, res: Response) => {
   
   const result = await pool.query(fruitQueries.getFruitAndNutritionQuery(fruit))
   res.json(result.rows)
-}
+})
 
 // Get the nutritional data for all fruits ()
-export const getFruitsAndNutrition = async (req: Request, res: Response) => {
+export const getFruitsAndNutrition = catchAsync(async (req: Request, res: Response) => {
   const result = await pool.query(fruitQueries.getFruitsAndNutritionQuery())
   res.json(result.rows)
-}
+})
