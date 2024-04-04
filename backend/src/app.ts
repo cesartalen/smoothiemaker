0f445789a7092831c176d500a1a4b9ed676a8597
cesartalen <cesartalense@gmail.com>
2024-04-04 21:13:35 +0200

    Add CORS configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,20 @@ import { router as smoothieRouter } from './routes/smoothieRoutes.js'
 import { router as fruitRouter } from './routes/fruitRoutes.js'
 import { errorHandler } from './middlewares/errorHandler.js';
 import rateLimit from 'express-rate-limit';
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://www.localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+)
 
 // Global rate limiter, limits the number of requests to 50 every 15 minutes for all routes
 export const sharedLimiter = rateLimit({
