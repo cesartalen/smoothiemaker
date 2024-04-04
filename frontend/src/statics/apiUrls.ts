907bb8311c6ef68d32a3072227435b29b59e7ab8
cesartalen <cesartalense@gmail.com>
2024-04-04 21:15:02 +0200

    Add Statics, Api URLs

---
+++ frontend/src/statics/apiUrls.ts
@@ -0,0 +1,7 @@
+// Api root, all api urls will be appended to this
+const API_ROOT = 'http://localhost:3000'
+
+// Fruit routes
+export const GET_ALL_FRUITS = `${API_ROOT}/fruits/fruits`
+
+// Smoothie routes
