bac3507e9245be9c732964694e76fab2da01c922
cesartalen <cesartalense@gmail.com>
2024-04-05 16:13:29 +0200

    Add functionality to retrieve fruits and nutritional data, and show it

--- frontend/src/pages/FruitsPage.tsx
+++ frontend/src/pages/FruitsPage.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react'
+import { getFruitsAndNutrition } from '../services/apiFruits'
+
 export const FruitsPage = () => {
+  const [fruitsData, setFruitsData] = useState<any>([])
+  const [selectedFruitId, setSelectedFruitId] = useState<number | null>(null)
+
+  const handleFruitClick = (fruitId: number) => {
+    setSelectedFruitId(selectedFruitId === fruitId ? null : fruitId)
+  }
+
+  useEffect(() => {
+    if(fruitsData.length === 0) {
+      getFruitsAndNutrition()
+      .then(data => setFruitsData(data))
+      .catch(err => console.error(err))
+    }
+  }, [])
+
   return (
     <div>
-      <h1>Fruits Page</h1>
+      <h1>All Fruits</h1>
+      {fruitsData ? (
+        <ul>
+          {fruitsData.map(fruit => (
+            <li key={fruit.id} onClick={() => handleFruitClick(fruit.id)}>{fruit.name}
+            {selectedFruitId === fruit.id && (
+              <div>
+                <p>Calories: {fruit.calories}</p>
+                <p>Sugar: {fruit.sugar}</p>
+                <p>Fat: {fruit.fat}</p>
+                <p>Carbs: {fruit.carbohydrates}</p>
+                <p>Protein: {fruit.protein}</p>
+              </div>
+            )}
+            </li>
+          ))}
+      	</ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
