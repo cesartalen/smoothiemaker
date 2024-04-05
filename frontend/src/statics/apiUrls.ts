9eeb7e1e8ce50e5ca6b02aca9e4ded33a5bbae06
cesartalen <cesartalense@gmail.com>
2024-04-05 16:12:12 +0200

    Add static url for getting all fruits and nutrition

--- frontend/src/statics/apiUrls.ts
+++ frontend/src/statics/apiUrls.ts
@@ -4,4 +4,6 @@ const API_ROOT = 'http://localhost:3000'
 // Fruit routes
 export const GET_ALL_FRUITS = `${API_ROOT}/fruits/fruits`
 
+export const GET_ALL_FRUITS_AND_NUTRITION = `${API_ROOT}/fruits/fruits-and-nutrition`
+
 // Smoothie routes
