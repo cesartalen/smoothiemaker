d41873dc60f93f1fe0ee15ec602935a98b3c5f88
cesartalen <cesartalense@gmail.com>
2024-04-05 16:12:37 +0200

    Add API call for get fruits and nutrition route

--- frontend/src/services/apiFruits.ts
+++ frontend/src/services/apiFruits.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
-import { GET_ALL_FRUITS } from '../statics/apiUrls'
+import { GET_ALL_FRUITS, GET_ALL_FRUITS_AND_NUTRITION } from '../statics/apiUrls'
 
 // API call to get all fruits
 export const getFruits = async () => {
   const response = await axios.get(GET_ALL_FRUITS)
+  return response.data
+}
+
+export const getFruitsAndNutrition = async () => {
+  const response = await axios.get(GET_ALL_FRUITS_AND_NUTRITION)
   return response.data
 }
