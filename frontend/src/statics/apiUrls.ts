15e7af190d09078bd54ef4579f58428963941310
cesartalen <cesartalense@gmail.com>
2024-04-09 18:40:09 +0200

    Add route for POST_SMOOTHIE_MIX

--- frontend/src/statics/apiUrls.ts
+++ frontend/src/statics/apiUrls.ts
@@ -1,5 +1,5 @@
 // Api root, all api urls will be appended to this
-const API_ROOT = 'http://localhost:3000'
+const API_ROOT = 'http://192.168.1.78:3000'
 
 // Fruit routes
 export const GET_ALL_FRUITS = `${API_ROOT}/fruits/fruits`
@@ -7,3 +7,4 @@ export const GET_ALL_FRUITS = `${API_ROOT}/fruits/fruits`
 export const GET_ALL_FRUITS_AND_NUTRITION = `${API_ROOT}/fruits/fruits-and-nutrition`
 
 // Smoothie routes
+export const POST_SMOOTHIE_MIX = `${API_ROOT}/smoothies/smoothie-mix`
