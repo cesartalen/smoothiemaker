9dfd88bfc24b9eab539b60716e2fe6464526beae
cesartalen <cesartalense@gmail.com>
2024-04-09 18:42:51 +0200

    Add postsmoothiemix on homepage for testing

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from 'react'
+import { postSmoothieMix } from '../services/apiSmoothies'
+
 export const HomePage = () => {
+
+  useEffect(() => {
+    postSmoothieMix().then(data => console.log(data))
+  }, [])
   return (
     <div>
       <h1>Home Page</h1>
