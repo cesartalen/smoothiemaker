c06da35c4e939a878e7d9417e431dd6469188c3f
cesartalen <cesartalense@gmail.com>
2024-04-09 19:36:46 +0200

    Add textarea, button, inputstate and handlers for both. Input gets split into array by comma and trimmed for spaces in start and end of words.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,36 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { postSmoothieMix } from '../services/apiSmoothies'
 
 export const HomePage = () => {
+  const [inputData, setInputData] = useState<string>()
+
+  const handleSubmit = async () => {
+    if(inputData != undefined) {
+      // TODO default amount should be 1, but if there is a number at the end before comma it should be used as the amount.
+
+      // Removes spaces in the beginning/end of each word, split them into arrays separated by comma.
+      let data = inputData.split(',').map(item => item.trim())
+    }
+
+    //TODO Notify user that nothing happens cause input is empty
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputData(e.target.value)
+  }
 
   useEffect(() => {
     postSmoothieMix().then(data => console.log(data))
   }, [])
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div>
+        <h1>Home Page</h1>
+      </div>
+      <div>
+        <textarea rows={4} cols={50} onChange={handleChange} placeholder='Separate fruits by comma'/>
+        <button onClick={() => handleSubmit()}></button>
+      </div>
+    </>
   )
 }
