fa11b3e4cb4f0d3c6a98cc3e5c755e478ebf1f04
cesartalen <cesartalense@gmail.com>
2024-04-19 14:07:27 +0200

    Add m-auto for centering

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header'
 
 export const AppLayout = () => {
   return(
-    <div className=' max-w-7xl text-center p-8'>
+    <div className=' max-w-7xl text-center p-8 m-auto'>
       <Header/>
       <Outlet/>
     </div>
