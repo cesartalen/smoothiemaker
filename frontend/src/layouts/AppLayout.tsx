da366c85e101e47ed980f709158e7da453f0cdcf
cesartalen <cesartalense@gmail.com>
2024-04-19 13:36:40 +0200

    Add changes to layout, center content, max width and padding

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header'
 
 export const AppLayout = () => {
   return(
-    <div>
+    <div className=' max-w-7xl text-center p-8'>
       <Header/>
       <Outlet/>
     </div>
